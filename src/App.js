aeab0b6498d2807d78e8b4762b97e3dd8428c847
Amar <codeking4u@gmail.com>
2020-03-05 18:01:15 +0530

    app design - weather and locatiom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d:Date) => {
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let currDay = days[d.getDay()];
+    let currMonth = months[d.getMonth()];
+    let currYear = d.getFullYear();
+    let currDate = d.getDate();
+    return `${currDay} ${currDate} ${currMonth} ${currYear}`
+  }
+
   return (
     <div className="App">
       <main>
@@ -16,6 +27,17 @@ function App() {
             placeholder = "Search City,Country"
           />
         </div>
+
+        <div className="location-box">
+          <div className = "location" >Goa, India</div>
+          <div className = "date" >{dateBuilder(new Date())}</div>
+        </div>
+
+        <div className= "weather-box">
+          <div className= "temp">15°C</div>
+          <div className= "weather">Sunny</div>
+        </div>
+
       </main>
     </div>
   );
