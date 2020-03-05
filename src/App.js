d4972e0480827f253c9bf935708d22ea5116847e
Amar <codeking4u@gmail.com>
2020-03-05 17:01:57 +0530

    search bar design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 //require('dotenv').config();
+const api = {
+  key: process.env.REACT_APP_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
-    <div className="App">asdas
-      {process.env.REACT_APP_KEY}
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className = "search-bar"
+            placeholder = "Search City,Country"
+          />
+        </div>
+      </main>
     </div>
   );
 }
